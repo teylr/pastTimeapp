ac7c9f08afc23aec9dcb0a9622b217f48a17fe5b
bl4nkst8 <swedenman260@gmail.com>
2024-11-21 00:34:00 +0000

    - created the MySQL database

--- picker.py
+++ picker.py
@@ -1,9 +1,13 @@
 from random import choice
 import pandas as pd
+import mysql.connector
 
 
-#use jikan for scraping data for anime
-#find another api for scraping manga
+#use jikan for scraping data for anime and then putting it into the database
+#pandas - for reading and extracting for the database
+#mysql - for the database
+#dear pygui - for making the application
+#find another api for books
 anime = ['naruto', 'onepiece','gintama','bleach','blue lock', 'seraph of the end']
 manga = ['boys abyss','berserk','one piece','etc']
 choiceOfPastTime = input("What do you want to do? ")
@@ -14,3 +18,21 @@ elif choiceOfPastTime == 'manga':
     print(choice(manga))
 else:
     print("Sorry, that's not something you can do")
+
+#sample sql connection
+
+
+# Connect to the MySQL database
+try:
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Homunculus24!",
+        database="jikan_db"
+    )
+    print("Connection successful!")
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
+finally:
+    if 'connection' in locals() and connection.is_connected():
+        connection.close()
